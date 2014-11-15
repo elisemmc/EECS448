afcf3f25bf89725c3fdff130d531dc405ac27518
elistax9 <e908m429@ku.edu>
2014-11-15 16:06:36 -0600

    IT WORKS

--- 448Homework#4/hw.py
+++ 448Homework#4/hw.py
@@ -1,8 +1,9 @@
 import fileinput
 
 class Book:
-	def __init__(self, ID, title, genre, price):
+	def __init__(self, ID, author, title, genre, price):
 		self.ID = ID
+		self.author = author
 		self.title = title
 		self.genre = genre
 		self.price = price
@@ -11,26 +12,26 @@ class BookList:
 	#all books in the list must have a unique ID
 	def __init__(self):
 		self.bookList = []
-	def addBook(self, ID, title, genre, price):
+	def addBook(self, ID, author, title, genre, price):
 		for index, item in enumerate(self.bookList):
-			if item.ID == ID:
-				return 'that ID is already being used'
+			if item.title == title:
+				return 'that title is already being used'
 		for index, item in enumerate(self.bookList):
-			if ID < item.ID:
-				self.bookList.insert(index, Book(ID, title, genre, price))
+			if title < item.title:
+				self.bookList.insert(index, Book(ID, author, title, genre, price))
 				return
-		self.bookList.append(Book(ID, title, genre, price))
+		self.bookList.append(Book(ID, author, title, genre, price))
 	def addBook2(self, Book):
-		self.addBook(Book.ID, Book.title, Book.genre, Book.price)
-	def removeBook(self, ID):
+		self.addBook(Book.ID, Book.author, Book.title, Book.genre, Book.price)
+	def removeBook(self, title):
 		if len(self.bookList) == 0:
 			print('no books in list')
 			return False
 		for index, item in enumerate(self.bookList):
-			if item.ID == ID:
+			if item.title == title:
 				self.bookList.pop(index)
 				return True
-		print('book ID not in list')
+		print('BOOK TITLE IS NOT IN LIST')
 		return False
 	def getTotalCost(self):
 		totalCost = 0
@@ -42,35 +43,76 @@ class BookList:
 
 def displayList(BookList):
 	for item in BookList.bookList:
-		print('ID: ' + str(item.ID))
+		#print('ID: ' + str(item.ID))
+		print('Author: ' + str(item.author))
 		print('Title: ' + str(item.title))
 		print('Genre: ' + str(item.genre))
-		print('Price: ' + str(item.price))
+		print('Price: $' + str(item.price))
 		print('--------')
 
 class Library:
 	def __init__(self):
 		self.List = BookList()
 		count = 1
-		for line in fileinput.input('Book-List-For-EECS-HW4.txt'):
-			if line.lstrip("\xef\xbb\xbf").rstrip() == "#":
-				print('hi')
-
+		for line in fileinput.input('SciFi($50).txt'):
 			if count == 1:
-				genre = line
+				genre = line.rstrip()
 				count = 2
-				print('genre set ' + line)
 			elif count == 2:
-				price = line
+				price = int(line.rstrip())
+				count = 3
+			elif count == 3:
+				ID = line
 				count = 4
-				print('price set ' + str(line))
-			else:
+			elif count == 4:
+				author = line.rstrip()
 				count = 5
+			elif count == 5:
+				title = line.rstrip()
+				self.List.addBook(ID, author, title, genre, price)
+				count = 3
+		count = 1
+		for line in fileinput.input('Travel($40).txt'):
+			if count == 1:
+				genre = line.rstrip()
+				count = 2
+			elif count == 2:
+				price = int(line.rstrip())
+				count = 3
+			elif count == 3:
+				ID = line
+				count = 4
+			elif count == 4:
+				author = line.rstrip()
+				count = 5
+			elif count == 5:
+				title = line.rstrip()
+				self.List.addBook(ID, author, title, genre, price)
+				count = 3
+		count = 1
+		for line in fileinput.input('SoftwareEngineering($100).txt'):
+			if count == 1:
+				genre = line.rstrip()
+				count = 2
+			elif count == 2:
+				price = int(line.rstrip())
+				count = 3
+			elif count == 3:
+				ID = line
+				count = 4
+			elif count == 4:
+				author = line.rstrip()
+				count = 5
+			elif count == 5:
+				title = line.rstrip()
+				self.List.addBook(ID, author, title, genre, price)
+				count = 3
 
-	def addBook(self, ID, title, genre, price):
-		self.List.addBook(ID, title, genre, price)
-	def removeBook(self, ID):
-		self.List.removeBook(ID)
+
+	def addBook(self, ID, author, title, genre, price):
+		self.List.addBook(ID, author, title, genre, price)
+	def removeBook(self, title):
+		self.List.removeBook(title)
 	def showList(self):
 		displayList(self.List)
 	def viewByGenre(self, genre):
@@ -80,24 +122,24 @@ class Library:
 				self.tempList.addBook2(item)
 		print('Viewing all ' + genre + ' books.')
 		displayList(self.tempList)
-	def getBookByID(self, ID):
+	def getBookBytitle(self, title):
 		for item in self.List.bookList:
-			if item.ID == ID:
+			if item.title == title:
 				return item
-		print('That book ID is not available')
+		print('THAT BOOK TITLE IS NOT AVAILABLE')
 		return False
 
 class Order:
 	def __init__(self):
 		self.List = BookList()
-	def addBook(self, Library, ID):
-		self.book = Library.getBookByID(ID)
+	def addBook(self, Library, title):
+		self.book = Library.getBookBytitle(title)
 		if self.book == False:
-			print('failed to add book')
+			print('FAILED TO ADD BOOK')
 			return
 		self.List.addBook2(self.book)
-	def removeBook(self, ID):
-		self.List.removeBook(ID)
+	def removeBook(self, title):
+		self.List.removeBook(title)
 	def viewList(self):
 		print('Viewing current order')
 		displayList(self.List)
@@ -107,31 +149,63 @@ class Order:
 class User_Controller:
 	#a user can only have one order
 	def __init__(self):
+		self.Lib = Library()
 		self.Order = Order()
+		selection = 0
+		while selection != '5':
+			print("Please choose an action:")
+			print("1 - View by Genre")
+			print("2 - Add to Order")
+			print("3 - Remove from Order")
+			print("4 - View Total Cost")
+			print("5 - Checkout")
+			selection = raw_input()
+			if selection == '1':
+				stuff = raw_input("What Genre would you like to view? ")
+				self.viewGenre(self.Lib, stuff)
+			elif selection == '2':
+				stuff = raw_input("What Title would you like to add? ")
+				self.addToOrder(self.Lib, stuff)
+			elif selection == '3':
+				stuff = raw_input("What Title would you like to remove? ")
+				self.removeFromOrder(stuff)
+			elif selection == '4':
+				self.viewTotalCost()
+			elif selection == '5':
+				self.viewOrder()
+				self.viewTotalCost()
 	def viewAll(self, Library):
 		Library.showList()
 	def viewGenre(self, Library, genre):
 		Library.viewByGenre(genre)
-	def addToOrder(self, Library, ID):
-		self.Order.addBook(Library, ID)
-	def removeFromOrder(self, ID):
-		self.Order.removeBook(ID)
+	def addToOrder(self, Library, title):
+		self.Order.addBook(Library, title)
+		self.viewOrder()
+	def removeFromOrder(self, title):
+		self.Order.removeBook(title)
+		self.viewOrder()
 	def viewOrder(self):
 		self.Order.viewList()
 	def viewTotalCost(self):
-		print('$' + str(self.Order.getTotalCost()))
+		print('TOTAL COST: $' + str(self.Order.getTotalCost()))
 
 
 
-Lib = Library()
+
+#Lib = Library()
 #Lib.addBook(1, 'a', 'Sci Fi', 2)
 #Lib.addBook(2, 'b', 'Sci Fi', 2)
 #Lib.addBook(3, 'c', 'Sci Fi', 2)
 #Lib.addBook(4, 'd', 'Horror', 4)
 #Lib.addBook(5, 'e', 'Horror', 4)
 #Lib.addBook(6, 'f', 'Horror', 4)
-#U = User_Controller()
+U = User_Controller()
 #U.viewAll(Lib)
-#U.viewGenre(Lib, 'Sci Fi')
-#U.addToOrder(Lib, 2)
+#U.viewGenre(Lib, 'Travel')
+#U.addToOrder(Lib, 'Arctic Dreams')
+#U.addToOrder(Lib, 'A Winter in Arabia')
+#U.addToOrder(Lib, 'The Forever War')
+#U.addToOrder(Lib, 'Code Complete: A Handbook of Software Construction')
+#U.removeFromOrder('The Forever War')
 #U.viewOrder()
+#U.viewTotalCost()
