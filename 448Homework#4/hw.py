dba33c39b95c9de7035a54e87aa77242f0436c70
elistax9 <e908m429@ku.edu>
2014-11-15 14:44:56 -0600

    edits

--- 448Homework#4/hw.py
+++ 448Homework#4/hw.py
@@ -1,3 +1,5 @@
+import fileinput
+
 class Book:
 	def __init__(self, ID, title, genre, price):
 		self.ID = ID
@@ -12,7 +14,7 @@ class BookList:
 	def addBook(self, ID, title, genre, price):
 		for index, item in enumerate(self.bookList):
 			if item.ID == ID:
-				return 'that ID is already in stock'
+				return 'that ID is already being used'
 		for index, item in enumerate(self.bookList):
 			if ID < item.ID:
 				self.bookList.insert(index, Book(ID, title, genre, price))
@@ -37,30 +39,47 @@ class BookList:
 		for index, item in enumerate(self.bookList):
 			totalCost = totalCost + item.price 
 		return totalCost
-	def displayList(self):
-		for item in self.bookList:
-			print('ID: ' + str(item.ID))
-			print('Title: ' + str(item.title))
-			print('Genre: ' + str(item.genre))
-			print('Price: ' + str(item.price))
-			print('--------')
+
+def displayList(BookList):
+	for item in BookList.bookList:
+		print('ID: ' + str(item.ID))
+		print('Title: ' + str(item.title))
+		print('Genre: ' + str(item.genre))
+		print('Price: ' + str(item.price))
+		print('--------')
 
 class Library:
 	def __init__(self):
 		self.List = BookList()
+		count = 1
+		for line in fileinput.input('Book-List-For-EECS-HW4.txt'):
+			if line.lstrip("\xef\xbb\xbf").rstrip() == "#":
+				print('hi')
+
+			if count == 1:
+				genre = line
+				count = 2
+				print('genre set ' + line)
+			elif count == 2:
+				price = line
+				count = 4
+				print('price set ' + str(line))
+			else:
+				count = 5
+
 	def addBook(self, ID, title, genre, price):
 		self.List.addBook(ID, title, genre, price)
 	def removeBook(self, ID):
 		self.List.removeBook(ID)
 	def showList(self):
-		self.List.displayList()
+		displayList(self.List)
 	def viewByGenre(self, genre):
 		self.tempList = BookList()
 		for item in self.List.bookList:
 			if item.genre == genre:
 				self.tempList.addBook2(item)
 		print('Viewing all ' + genre + ' books.')
-		self.tempList.displayList()
+		displayList(self.tempList)
 	def getBookByID(self, ID):
 		for item in self.List.bookList:
 			if item.ID == ID:
@@ -81,11 +100,11 @@ class Order:
 		self.List.removeBook(ID)
 	def viewList(self):
 		print('Viewing current order')
-		self.List.displayList()
+		displayList(self.List)
 	def getTotalCost(self):
 		return self.List.getTotalCost()
 
-class User:
+class User_Controller:
 	#a user can only have one order
 	def __init__(self):
 		self.Order = Order()
@@ -102,40 +121,17 @@ class User:
 	def viewTotalCost(self):
 		print('$' + str(self.Order.getTotalCost()))
 
+
+
 Lib = Library()
-Lib.addBook(1, 'a', 'Sci Fi', 2)
-Lib.addBook(2, 'b', 'Sci Fi', 2)
-Lib.addBook(3, 'c', 'Sci Fi', 2)
-Lib.addBook(4, 'd', 'Horror', 4)
-Lib.addBook(5, 'e', 'Horror', 4)
-Lib.addBook(6, 'f', 'Horror', 4)
-Lib.addBook(7, 'g', 'Fantasy', 6)
-Lib.addBook(8, 'h', 'Fantasy', 6)
-Lib.addBook(9, 'i', 'Fantasy', 6)
-Lib.addBook(10, 'j', 'Non-fiction', 8)
-Lib.addBook(11, 'k', 'Non-fiction', 8)
-Lib.addBook(12, 'l', 'Non-fiction', 8)
-P = User()
-P.viewAll(Lib)
-P.viewGenre(Lib, 'Horror')
-P.viewGenre(Lib, 'Non-fiction')
-P.addToOrder(Lib, 2)
-P.addToOrder(Lib, 3)
-P.addToOrder(Lib, 4)
-P.addToOrder(Lib, 5)
-P.addToOrder(Lib, 8)
-P.addToOrder(Lib, 12)
-P.removeFromOrder(3)
-P.viewOrder()
-P.viewTotalCost()
-
-
-
-
-
-
-
-
-
-
-
+#Lib.addBook(1, 'a', 'Sci Fi', 2)
+#Lib.addBook(2, 'b', 'Sci Fi', 2)
+#Lib.addBook(3, 'c', 'Sci Fi', 2)
+#Lib.addBook(4, 'd', 'Horror', 4)
+#Lib.addBook(5, 'e', 'Horror', 4)
+#Lib.addBook(6, 'f', 'Horror', 4)
+#U = User_Controller()
+#U.viewAll(Lib)
+#U.viewGenre(Lib, 'Sci Fi')
+#U.addToOrder(Lib, 2)
+#U.viewOrder()
